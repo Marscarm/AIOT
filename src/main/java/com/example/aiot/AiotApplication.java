e8fd064b075470a91a4eb57d8c84fd12c5f9cb62
zichen.dang <zichen.dang@Sophgo.com>
2023-05-30 15:18:59 +0800

    init

---
+++ src/main/java/com/example/aiot/AiotApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiotApplication.class, args);
+    }
+
+}
